b46053963f017d8e1c23cb23e59e45375bf44aee
Elijah Nightengale <elijah.charles.smile@gmail.com>
2017-10-22 16:43:22 -0400

    Updated app content

--- js/script.js
+++ js/script.js
@@ -3,84 +3,104 @@ var box_click = document.querySelector(".col-2");
 var matched = document.querySelector(".matched");
 const num_of_boxes = 16;
 let boxes;
-var symbols = [2, 3, 3, 2, 1, 1, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8];
+var symbols = [13, 13, 3, 3, 1, 1, 4, 4, 5, 5, 10, 10, 7, 7, 8, 8];
 let clicked = -1;
 var values = [];
 var clicked_boxes = [];
-
 var moves = document.querySelector("span");
 let moves_in_game = 0;
+var correct_boxes = [];
 
 
+//shuffle array of numbers
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
 
-//function for boxes that determines matches
-function handler(e){
-  moves_in_game = moves_in_game + 1;
-  moves.innerHTML = moves_in_game;
-  clicked = clicked + 1;
-  var value = this.innerHTML;
-  values.push(value);
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
 
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
 
-  if(clicked === 2){
-    $(this).addClass("fade z-index");
-    twoClicked();
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
   }
 
-  $(this).addClass("fade z-index");
+  return array;
+}
 
-  if(clicked === 1 && a_match()){
 
+
+//create cards
+var create_cards = function(card){
+
+var cards = shuffle(symbols);
+for(var i = 0; i < cards.length; i++){
+  var symbol = i;
+}
+
+  for (var i = 0; i < num_of_boxes; i++) {
+
+    boxes = document.createElement("div");
+    $(boxes).addClass("col-2");
+    boxes.innerHTML = i;
+    var opened_boxes = [];
+
+
+    boxes.addEventListener("click", function(){
+      moves_in_game = moves_in_game + 1;
+      moves.innerHTML = moves_in_game;
+
+
+
+      $(this).addClass("fade z-index");
+      clicked++;
+      console.log(clicked);
+      opened_boxes.push(this);
+      console.log(opened_boxes);
+      var box_one = opened_boxes[0];
+      var box_two = opened_boxes[1];
+      var box_one_value = box_one.innerHTML;
+      var box_two_value = box_two.innerHTML;
+
+
+      if(box_one_value === box_two_value) {
+
+        correct_boxes.push(box_one_value);
+        correct_boxes.push(box_two_value);
+
+        $(box_one).addClass("correct");
+        $(box_two).addClass("correct");
+
+        opened_boxes = [];
+
+        setTimeout(function(){
+          $("div").removeClass("fade z-index wrong");
+          clicked = -1;
+
+        }, 1500);
+
+      }
+
+      if(clicked === 1 && opened_boxes.length === 2) {
+
+        $(box_one).addClass("wrong");
+        $(box_two).addClass("wrong");
+
+        opened_boxes = [];
+
+        setTimeout(function(){
+          $("div").removeClass("fade z-index wrong");
+          clicked = -1;
+
+        }, 1500);
+      }
+    });
+    parent.appendChild(boxes);
   }
 }
 
-//adds eventListener for boxes
-var box_clicked = function(box){
-
-  box.addEventListener("click", handler, true);
-  var this_box = box;
-  clicked_boxes.push(this_box);
-
-}
-
-//creates the boxes and adds classes, numbers, and event listeners to them.
-for(let i = 0; i < num_of_boxes; i++){
-  var symbol = symbols[Math.floor(Math.random() * symbols.length)];
-
-  boxes = document.createElement("div");
-  box_clicked(boxes);
-  boxes.innerHTML = symbol;
-  $(boxes).addClass("col-2");
-  parent.appendChild(boxes);
-
-}
-
-
-
-//a function that doesn't allow users to see more than two squares!
-var twoClicked = function(){
-  $("div").removeClass("fade z-index");
-  clicked = 0;
-  var third_value = values.pop(2);
-
-  values = [];
-  values.push(third_value);
-
-}
-
-//DECTECTS A MATCH IN THE GAME
-var a_match = function(box) {
-  var x = values[0];
-  var y = values[1];
-  var match_one = clicked_boxes.length - 1;
-  var match_two = clicked_boxes.length - 2;
-  var fade_out = clicked_boxes[match_one];
-  var fade_out_two = clicked_boxes[match_two];
-
-  if(x === y){
-    fade_out.removeEventListener("click", handler, false);
-    fade_out_two.removeEventListener("click", handler, false);
-    $(fade_out).addClass("matched");
-    $(fade_out_two).addClass("matched");
-  }
-}
+create_cards(boxes);
---
+++ type.js
@@ -0,0 +1,115 @@
+let parent = document.querySelector(".father");
+var box_click = document.querySelector(".col-2");
+var matched = document.querySelector(".matched");
+const num_of_boxes = 16;
+let boxes;
+var symbols = [13, 13, 3, 3, 1, 1, 4, 4, 5, 5, 10, 10, 7, 7, 8, 8];
+let clicked = -1;
+var values = [];
+var clicked_boxes = [];
+var moves = document.querySelector("span");
+let moves_in_game = 0;
+var correct_boxes = [];
+var cards;
+var close = document.querySelector("button");
+var modal = document.querySelector("#modal")
+
+
+
+//shuffle array of numbers got this from stackoverflow
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
+var create_shuffle_cards = function(){
+
+  cards = shuffle(symbols);
+    for(var i = 0; i < cards.length; i++){
+      boxes = document.createElement("div");
+      $(boxes).addClass("col-2");
+      boxes.innerHTML = cards[i];
+      parent.appendChild(boxes);
+    }
+}
+
+//create cards
+var game = function(card){
+
+  create_shuffle_cards();
+
+  //show symbol when card is clicked and increment moves in game
+  $(".col-2").click(function(){
+    $(this).addClass("fade z-index");
+    moves_in_game = moves_in_game + 1;
+    moves.innerHTML = moves_in_game;
+    clicked_boxes.push(this);
+
+
+    var x = clicked_boxes[0];
+    var y = clicked_boxes[1];
+    
+
+    //does x and y match? correct boxes
+    if(x.innerHTML === y.innerHTML){
+      correct_boxes.push(x);
+      correct_boxes.push(y);
+
+      if(correct_boxes.length === 16){
+
+        $("div#modal.modal").css("display", "block");
+
+      }
+
+      $(x).addClass("correct");
+      $(y).addClass("correct");
+    }
+
+    //reset clicked_boxes/wrong matches
+    if(clicked_boxes.length === 2) {
+      $(x).addClass("wrong");
+      $(y).addClass("wrong");
+      clicked_boxes = [];
+      setTimeout(function(){
+
+        $(x).removeClass("fade z-index wrong");
+        $(y).removeClass("fade z-index wrong");
+
+      }, 1000);
+    }
+  });
+
+
+  //restart game
+    $("i").click(function(){
+      $("div").removeClass("fade z-index wrong correct");
+      moves_in_game = 0;
+      moves.innerHTML = moves_in_game;
+    });
+}
+
+
+//modal functionality
+$(".modal-content").css("display", "none");
+$("div#modal.modal").css("display", "none");
+close.onclick = function() {
+    $(modal).css("display", "none");
+}
+
+
+
+game(boxes);
